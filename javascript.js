7230f13be8933353e336179c823c890d7ccfca4e
rpereira-p <103273626+rpereira-p@users.noreply.github.com>
2022-06-08 16:34:54 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let contador = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 3000)
+
+
+function nextImage(){
+    contador++;
+    if(contador>4){
+        contador = 1; 
+    }
+
+    document.getElementById("radio"+contador).checked = true;
+}
